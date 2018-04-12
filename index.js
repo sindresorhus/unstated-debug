580367ac67e214f41a520dccea2f8d10750dcb91
Jamie Kyle <me@thejameskyle.com>
2018-04-12 15:32:24 -0700

    2

--- index.js
+++ index.js
@@ -1,82 +1,66 @@
 'use strict';
 const {detailedDiff} = require('deep-object-diff');
+const {__SUPER_SECRET_CONTAINER_DEBUG_HOOK__} = require('unstated');
 
-module.exports = options => {
-	options = {
-		isEnabled: true,
-		logStateChanges: true,
-		...options
-	};
-
-	return Container => {
-		if (!options.isEnabled) {
-			return Container;
+const UNSTATED = {
+	isEnabled: true,
+	logStateChanges: true,
+	containers: {},
+	get states() {
+		const ret = {};
+		for (const [key, value] of Object.entries(this.containers)) {
+			ret[key] = value.state;
 		}
-
-		if (!window.UNSTATED) {
-			window.UNSTATED = {
-				isEnabled: true,
-				logStateChanges: options.logStateChanges,
-				containers: {},
-
-				get states() {
-					const ret = {};
-					for (const [key, value] of Object.entries(this.containers)) {
-						ret[key] = value.state;
-					}
-					return ret;
-				},
-
-				logState() {
-					for (const [key, value] of Object.entries(this.containers)) {
-						console.log(`%c${key}\n`, 'font-weight:bold', value.state);
-					}
-				}
-			};
+		return ret;
+	},
+	logState() {
+		for (const [key, value] of Object.entries(this.containers)) {
+			console.log(`%c${key}\n`, 'font-weight:bold', value.state);
 		}
-
-		const globalInstance = window.UNSTATED;
-		const logStateChangeKey = Symbol('log state key');
-
-		class DebuggableContainer extends Container {
-			[logStateChangeKey](state) {
-				const {name} = this.constructor;
-				const diff = detailedDiff(this.state, state);
-
-				console.group(name);
-
-				if (diff.added) {
-					console.log('Added\n', diff.added);
-				}
-
-				if (diff.updated) {
-					console.log('Updated\n', diff.updated);
-				}
-
-				if (diff.deleted) {
-					console.log('Deleted\n', diff.deleted);
-				}
-
-				console.log('New state\n', state);
-				console.log('Old state\n', this.state);
-
-				console.groupEnd(name);
-			}
-
-			constructor(...args) {
-				super(...args);
-				globalInstance.containers[this.constructor.name] = this;
-			}
-
-			setState(state) {
-				if (globalInstance.isEnabled && globalInstance.logStateChanges) {
-					this[logStateChangeKey](state);
-				}
-
-				super.setState(state);
-			}
-		}
-
-		return DebuggableContainer;
-	};
+	}
 };
+
+__SUPER_SECRET_CONTAINER_DEBUG_HOOK__(container => {
+	if (!UNSTATED.isEnabled) {
+		return;
+	}
+
+	const {name} = container.constructor;
+
+	UNSTATED.containers[name] = container;
+
+	let prevState = container.state;
+
+	container.subscribe(() => {
+		if (!(UNSTATED.isEnabled && UNSTATED.logStateChanges)) {
+			return;
+		}
+
+		const {state} = container;
+		const diff = detailedDiff(prevState, state);
+
+		console.group(name);
+
+		if (diff.added) {
+			console.log('Added\n', diff.added);
+		}
+
+		if (diff.updated) {
+			console.log('Updated\n', diff.updated);
+		}
+
+		if (diff.deleted) {
+			console.log('Deleted\n', diff.deleted);
+		}
+
+		console.log('New state\n', state);
+		console.log('Old state\n', prevState);
+
+		console.groupEnd(name);
+
+		prevState = state;
+	});
+});
+
+window.UNSTATED = UNSTATED;
+module.exports = UNSTATED;
